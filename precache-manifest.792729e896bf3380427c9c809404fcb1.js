2bc57e160114c1a9c6aab4470d7ffa206be83a21
Denis Kochetov <57500456+DenisKochetov@users.noreply.github.com>
2020-10-22 14:39:18 +0500

    Updates

---
+++ precache-manifest.792729e896bf3380427c9c809404fcb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ae041cc41617e4873bd84edc5f89e2",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "01330f9fc3d15605e896",
+    "url": "/quiz/static/css/main.15130cd8.chunk.css"
+  },
+  {
+    "revision": "3d2422d67398c407d1ba",
+    "url": "/quiz/static/js/2.06c8429c.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/quiz/static/js/2.06c8429c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01330f9fc3d15605e896",
+    "url": "/quiz/static/js/main.06baf856.chunk.js"
+  },
+  {
+    "revision": "92182ec1fb6d434d2e02",
+    "url": "/quiz/static/js/runtime-main.d4225ac9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quiz/static/media/logo.5d5d9eef.svg"
+  }
+]);
